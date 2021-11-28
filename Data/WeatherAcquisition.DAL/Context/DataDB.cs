1c47c878752db3a1c92beea987f85e120ce899ea
ZamanovRenat <zamanov.renat@mail.ru>
2021-11-28 19:16:13 +0300

    Объявление базовых сущностей

---
+++ Data/WeatherAcquisition.DAL/Context/DataDB.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WeatherAcquisition.DAL.Context
+{
+    public class DataDB : DbContext
+    {
+        public DataDB(DbContextOptions<DataDB> options) : base(options) { }
+    }
+}
