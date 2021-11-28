a6e4a9916ed4c72f3c63c97b6fa825dd406f95d5
ZamanovRenat <zamanov.renat@mail.ru>
2021-11-28 18:19:48 +0300

    Подключение EintityFramework

---
+++ Data/WeatherAcquisition.DAL.SqlServer/Context/DataDB.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WeatherAcquisition.DAL.SqlServer.Context
+{
+    public class DataDB : DbContext
+    {
+        public DataDB() : base()
+        {
+            
+        }
+    }
+}
