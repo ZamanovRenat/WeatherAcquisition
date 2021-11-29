351dab1483699a77216068cc370bbfc69f69ca11
ZamanovRenat <zamanov.renat@mail.ru>
2021-11-29 21:26:29 +0300

    Регистрация репозиториев

--- Services/WeatherAcquisition.API/Startup.cs
+++ Services/WeatherAcquisition.API/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using WeatherAcquisition.API.Data;
 using WeatherAcquisition.DAL.Context;
+using WeatherAcquisition.DAL.Entities;
+using WeatherAcquisition.DAL.Repositories;
+using WeatherAcquisition.Interfaces.Base.Repositories;
 
 namespace WeatherAcquisition.API
 {
@@ -20,6 +23,10 @@ namespace WeatherAcquisition.API
                         configuration.GetConnectionString("Data"),
                         o => o.MigrationsAssembly("WeatherAcquisition.DAL.SqlServer")));
             services.AddTransient<DataDBInitializer>();
+
+            services.AddScoped(typeof(IRepository<>), typeof(DbRepository<>));
+            services.AddScoped(typeof(INamedRepository<>), typeof(DbNamedRepository<>));
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
