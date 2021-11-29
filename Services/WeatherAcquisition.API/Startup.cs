d94fcf6fc9447a30aa70252896e2cc9c50c2fc47
ZamanovRenat <zamanov.renat@mail.ru>
2021-11-29 21:22:35 +0300

    Наполнение БД тестовыми данными

--- Services/WeatherAcquisition.API/Startup.cs
+++ Services/WeatherAcquisition.API/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using WeatherAcquisition.API.Data;
 using WeatherAcquisition.DAL.Context;
 
 namespace WeatherAcquisition.API
@@ -18,7 +19,7 @@ namespace WeatherAcquisition.API
                     .UseSqlServer(
                         configuration.GetConnectionString("Data"),
                         o => o.MigrationsAssembly("WeatherAcquisition.DAL.SqlServer")));
-            
+            services.AddTransient<DataDBInitializer>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -27,8 +28,9 @@ namespace WeatherAcquisition.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataDBInitializer db)
         {
+            db.Initialize();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
