d563251d75daaefc3b282a285241e8dceb9fe087
ZamanovRenat <zamanov.renat@mail.ru>
2021-11-30 21:12:02 +0300

    Доработка логики контроллера репозитория DataSource

--- Services/WeatherAcquisition.API/Controllers/DataSourcesController.cs
+++ Services/WeatherAcquisition.API/Controllers/DataSourcesController.cs
@@ -24,6 +24,14 @@ namespace WeatherAcquisition.API.Controllers
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
         [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(bool))]
         public async Task<IActionResult> ExistId(int id) => await _repository.ExistId(id) ? Ok(true) : NotFound(false);
+        
+        //Проверка наличия объекта
+        [HttpGet("exist")]
+        [HttpPost("exist")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(bool))]
+        public async Task<IActionResult> Exist(DataSource item) => await _repository.Exist(item) ? Ok(true) : NotFound(false);
+
         //Получение всех объектов
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -42,6 +50,75 @@ namespace WeatherAcquisition.API.Controllers
         {
             var result = await _repository.GetPage(PageIndex, PageSize);
             return result.Items.Any() ? Ok(result) : NotFound(result);
+        }
+
+        /// <summary>
+        /// Получение объектов по Id
+        /// </summary>
+        /// <param name="id">Id объекта</param>
+        /// <returns></returns>
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id) => 
+            await _repository.GetById(id) is { } item 
+                ? Ok(item) 
+                : NotFound();
+
+        /// <summary>
+        /// Добавление объекта
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Add(DataSource item)
+        {
+            var result = await _repository.Add(item);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id });
+        }
+        /// <summary>
+        /// Редактирование объекта
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update(DataSource item)
+        {
+            if (await _repository.Update(item) is not { } result)
+                return NotFound(item);
+            return AcceptedAtAction(nameof(GetById), new { id = result.Id });
+        }
+        /// <summary>
+        /// Удаление объекта
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(DataSource item)
+        {
+            if (await _repository.Delete(item) is not { } result)
+                return NotFound(item);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Удаление объекта по идентификатору
+        /// </summary>
+        /// <param name="id">идентификатор</param>
+        /// <returns></returns>
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteById(int id)
+        {
+            if (await _repository.DeleteById(id) is not { } result)
+                return NotFound(id);
+            return Ok(result);
         }
     }
 }
